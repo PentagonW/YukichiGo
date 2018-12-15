6c7165d3b6e4474df28d862371403b967f902a58
kurosawa <kuro6412@gmail.com>
2018-12-15 21:49:21 +0900

    ユキチ名を変更できる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,23 @@ class UsersController < ApplicationController
     @user = User.find params[:id]
     @monsters = @user.monsters.order(created_at: :desc).page(params[:page])
   end
+
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update user_params
+      redirect_to user_path @user.id
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   resources :monsters
   resources :battles, only: [:index, :new]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 
   namespace :api do
     namespace :v1 do
