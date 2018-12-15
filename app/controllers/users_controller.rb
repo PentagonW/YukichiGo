bfd4ff5aeae58e705ab65d84cebaab5701b6c793
kurosawa <kuro6412@gmail.com>
2018-12-15 21:34:15 +0900

    paginate追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
-    @monsters = @user.monsters
+    @monsters = @user.monsters.order(created_at: :desc).page(params[:page])
   end
 end
