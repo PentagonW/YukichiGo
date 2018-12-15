0cca42729e7fb760c8c8f1a32836c1da8c703214
KotaSakurai <sakurai.kota@mwed.co.jp>
2018-12-15 16:42:58 +0900

    add form

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,4 +1,15 @@
 class MonstersController < ApplicationController
   def new
+    @moster_form = MonsterForm.new
   end
+
+  def create
+    @moster_form = MonsterForm.new(form_params)
+  end
+
+  private
+
+    def form_params
+      params.permit(:file)
+    end
 end
