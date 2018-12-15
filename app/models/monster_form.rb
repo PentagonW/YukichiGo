8e89681ae661fe6d2db8d3b69ee3d7039677418a
yasuke <yasukexxx@gmail.com>
2018-12-15 23:52:34 +0900

    Set main monster

--- app/models/monster_form.rb
+++ app/models/monster_form.rb
@@ -6,29 +6,32 @@ class MonsterForm
   attr_accessor :file, :detector, :user_id, :serial_number, :noguchi
 
   def save
-    begin
-      get_serial_number
+    get_serial_number
 
-      if Monster.find_by(serial_number: serial_number)
-        flash[:error] = "既にいるのぐちです。"
-        return false
-      end
-
-      power = PowerCalculator.new(serial_number).power
-
-      @noguchi = Monster.new(name: DEFAULT_NAME, serial_number: serial_number, power: power, user_id: user_id)
-      @noguchi.save
-
-      AddAdjective.new(noguchi).update_adjective
-      true
-    rescue
-      false
+    if Monster.find_by(serial_number: serial_number)
+      flash[:error] = "既にいるのぐちです。"
+      return false
     end
+
+    power = PowerCalculator.new(serial_number).power
+
+    @noguchi = Monster.new(name: DEFAULT_NAME, serial_number: serial_number, power: power, user_id: user_id)
+    @noguchi.save
+    user.update! main_monster: @noguchi
+
+    AddAdjective.new(noguchi).update_adjective
+    true
+  rescue
+    false
   end
 
   def get_serial_number
     detector = TextDetector.new(file: file)
     detector.detect
     @serial_number = detector.serial_number
+  end
+
+  def user
+    @user ||= User.find user_id
   end
 end
