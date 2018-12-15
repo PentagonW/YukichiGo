4745da5775f36806ad2d904ff8294800e80699cc
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-12-15 21:37:41 +0900

    add: redirect

--- app/javascript/packs/add_monster.js
+++ app/javascript/packs/add_monster.js
@@ -1,15 +1,6 @@
 $(() => {
-  function flash(message, type = "success") {
-    const flash_box = $('<div>').attr({
-      class: `alert alert-${type} m-2`,
-    });
-    flash_box.text(message);
-    $("#flash").append(flash_box);
-    setTimeout(() => { flash_box.remove() }, 5000);
-  }
-
-  $('input[type=file]').change(function() {
-    const $form = $(".js-monster-form");
+  const $form = $(".js-monster-form");
+  $form.on("change", () => {
     $form.submit();
-  });
+  })
 });
