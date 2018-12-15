c2c91e79d0de4a7f13f3bb6998e431b385ab9d49
kurosawa <kuro6412@gmail.com>
2018-12-15 16:46:45 +0900

    テーブル定義修正

--- db/migrate/20181215072630_add_refarence_monster.rb
+++ db/migrate/20181215072630_add_refarence_monster.rb
@@ -1,7 +1,10 @@
 class AddRefarenceMonster < ActiveRecord::Migration[5.2]
   def change
-    add_column :monsters, :user_id, :integer, null: false
-    add_column :monsters, :created_at, :datetime, null: false
-    add_column :monsters, :updated_at, :datetime, null: false
+    add_column :monsters, :user_id, :integer
+    change_column :monsters, :user_id, :integer, null: false
+    add_column :monsters, :created_at, :datetime
+    change_column :monsters, :created_at, :integer, null: false
+    add_column :monsters, :updated_at, :datetime
+    change_column :monsters, :updated_at, :integer, null: false
   end
 end
