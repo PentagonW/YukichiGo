ca236bc6359198932046f63c5c21f2a54c9d214f
yasuke <yasukexxx@gmail.com>
2018-12-15 22:34:37 +0900

    Add noguchi battle

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,9 +1,12 @@
 class BattlesController < ApplicationController
   def index
-    @win_battles = current_user.win_battles
+j   @win_battles = current_user.win_battles
     @lose_battles = current_user.lose_battles
   end
 
   def new
+    @opponent = User.find params[:user_id]
+    @judge = BattleJudge.new users: [current_user, @opponent]
+    redirect_to root_path unless @judge.save
   end
 end
