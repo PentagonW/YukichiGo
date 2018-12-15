000c2dfb9a47181a950d3a336200d957e81ddf87
yasuke <yasukexxx@gmail.com>
2018-12-16 07:25:54 +0900

    add battle rate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   has_many :lose_battles, class_name: "::Battle", foreign_key: "loser_id", dependent: :destroy
   has_many :money_histories, class_name: "::MoneyHistory", dependent: :destroy
 
+  def battle_rate
+    return 0 if (win_battles.count + lose_battles.count).zero?
+
+    @battle_rate ||= win_battles.count / (win_battles.count + lose_battles.count).to_f * 100
+  end
+
   # 所持金
   def money_in_hand
     money + money_histories.pluck(:price).sum
