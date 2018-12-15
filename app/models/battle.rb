a0f552132f00cd5c440e0c1e3c011727eb991999
RyosukeHamaguchi <soccer1356abc@gmail.com>
2018-12-15 21:56:08 +0900

    Fix association around battle

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,5 +1,5 @@
 class Battle < ApplicationRecord
   belongs_to :user
-  belongs_to :winner, class_name: "::Monster", foreign_key: "winner_id"
-  belongs_to :loser, class_name: "::Monster", foreign_key: "loser_id"
+  belongs_to :winner, class_name: "::User", foreign_key: "winner_id"
+  belongs_to :loser, class_name: "::User", foreign_key: "loser_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :monsters
   belongs_to :main_monster, class_name: "Monster", required: false
-  has_many :battles, dependent: :destroy
+  has_many :win_battles, class_name: "::Battle", foreign_key: "winner_id", dependent: :destroy
+  has_many :lose_battles, class_name: "::Battle", foreign_key: "loser_id", dependent: :destroy
 end
