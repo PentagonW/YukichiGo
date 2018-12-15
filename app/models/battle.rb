f937f52b2c53221e4b7ab6a218b57048d0cd7861
yasuke <yasukexxx@gmail.com>
2018-12-15 22:50:24 +0900

    Fix battle

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,5 +1,4 @@
 class Battle < ApplicationRecord
-  belongs_to :user
   belongs_to :winner, class_name: "::User", foreign_key: "winner_id"
   belongs_to :loser, class_name: "::User", foreign_key: "loser_id"
 end
