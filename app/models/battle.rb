ef9b17d16d5314cb819a39fa4def513f1d70bf59
RyosukeHamaguchi <soccer1356abc@gmail.com>
2018-12-15 18:29:41 +0900

    Add association around battle

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -1,2 +1,5 @@
 class Battle < ApplicationRecord
+  belongs_to :user
+  belongs_to :winner, class_name: "::Monster", foreign_key: "winner_id"
+  belongs_to :lower, class_name: "::Monster", foreign_key: "loser_id"
 end
