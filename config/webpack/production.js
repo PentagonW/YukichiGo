af3cc32b45eb126f1d76b78eb3d2b257c7ce61fe
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-12-16 11:17:26 +0900

    add: production

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+environment.plugins.append('UglifyJs', new UglifyJsPlugin())
 
 module.exports = environment.toWebpackConfig()
