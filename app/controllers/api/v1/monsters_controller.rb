849a90eb5bfc7fefcf907dd9d2963c67799a2a9e
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-12-16 10:18:32 +0900

    add

--- app/controllers/api/v1/monsters_controller.rb
+++ app/controllers/api/v1/monsters_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::MonstersController < ApplicationController
   def create
     power = PowerCalculator.new(randomize).power
-    @monster = Monster.new(ecology: :wild, power: power, serial_number: randomize)
+    @monster = Monster.new(ecology: :wild, power: power, serial_number: randomize, name: "ノグチ")
     @monster.build_ability(class_type: rand(3))
     @monster.save!
     AddAdjective.new(@monster).update_adjective
