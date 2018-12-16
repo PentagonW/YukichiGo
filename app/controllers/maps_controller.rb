e5d52a907aa2fcbc75d3113ede7390ecced2e891
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-12-16 10:29:17 +0900

    redirect mypage

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,15 @@
 class MapsController < ApplicationController
+  before_action :required_main_monster
+
   def index
   end
+
+  private
+
+    def required_main_monster
+      unless current_user.main_monster
+        flash[:error] = "ユーザー情報のノグチ一覧から、メインノグチを登録してください"
+        redirect_to user_path(current_user)
+      end
+    end
 end
