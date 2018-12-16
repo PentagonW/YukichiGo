a519153612d221881565f0286b5ff4aa4299b8eb
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-12-16 10:44:58 +0900

    fixed: battle

--- app/controllers/wild_battles_controller.rb
+++ app/controllers/wild_battles_controller.rb
@@ -5,7 +5,7 @@ class WildBattlesController < ApplicationController
     # 勝った場合はゲット
     if @judge.winner?
       @wild_monster.user = current_user
-      @wild_monster.save
+      @wild_monster.save!
     end
   end
 end
