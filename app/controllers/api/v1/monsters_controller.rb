281dc997893d8fbc0dc7697f1ea0f789c85709c3
kazumalab <kazuma-arashi@hotmail.co.jp>
2018-12-16 09:05:58 +0900

    add: wild monster

---
+++ app/controllers/api/v1/monsters_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::MonstersController < ApplicationController
+  def create
+    power = PowerCalculator.new(randomize).power
+    @monster = Monster.new(ecology: :wild, power: power)
+    @monster.build_ability(class_type: rand(3))
+    @monster.save!
+    AddAdjective.new(@monster).update_adjective
+    render json: { id: @monster.id }
+  end
+
+  private
+
+    def randomize
+      SecureRandom.hex(4).upcase + ("A".."Z").to_a.sample
+    end
+end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,6 +1,7 @@
 class Monster < ApplicationRecord
   has_one :ability, dependent: :destroy, inverse_of: :monster
-  belongs_to :user
+  belongs_to :user, require: false
+  enum ecology: { reading: 0, wild: 1 }
 
   delegate :class_type, to: :ability, allow_nil: true
 
