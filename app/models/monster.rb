fa19ccb0a23261213bdd189d4cd2f2d47402b575
yasuke <yasukexxx@gmail.com>
2018-12-16 11:10:50 +0900

    Add experience

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -8,4 +8,12 @@ class Monster < ApplicationRecord
   def display_name
     name || "ノグチ"
   end
+
+  def update_experience!(opponent, judge)
+    calculator = ExperienceCalculator.new monster: self, opponent: opponent, judge: judge
+    self.experience_value += calculator.gain
+    self.level += experience_value / experience_value_limit
+    self.experience_value %= experience_value_limit
+    save!
+  end
 end
