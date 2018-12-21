2783397fe3e36212eb3bac21998dc4626ec908e2
yasuke <yasukexxx@gmail.com>
2018-12-21 22:07:37 +0900

    Fix battle error

--- app/controllers/battles_controller.rb
+++ app/controllers/battles_controller.rb
@@ -1,4 +1,6 @@
 class BattlesController < ApplicationController
+  before_action :have_main_monster
+
   def index
     @win_battles = current_user.win_battles.order(created_at: :desc).page(params[:win_page])
     @lose_battles = current_user.lose_battles.order(created_at: :desc).page(params[:lose_page])
@@ -11,4 +13,13 @@ class BattlesController < ApplicationController
     @experience_form = ExperienceForm.new winner: @judge.winner.main_monster, loser: @judge.loser.main_monster
     @experience_form.save
   end
+
+  private
+
+    def have_main_monster
+      unless current_user.main_monster
+        flash[:error] = "まずはノグチをGETしよう！"
+        redirect_to root_path 
+      end
+    end
 end
